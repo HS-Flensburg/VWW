20b0726413cd6ef865d9e04044b6973d7c8b5ae2
Benjamin Schulz <bschulz@code-area.net>
2016-03-31 14:30:20 +0200

    Delegates / higher order functions

--- VWW_Vorlesung/VWW_V2/Program.cs
+++ VWW_Vorlesung/VWW_V2/Program.cs
@@ -8,8 +8,33 @@ namespace VWW_V2
 {
     class Program
     {
+        delegate bool Predicate(int i);
+
         static void Main(string[] args)
         {
+            Predicate myFunc = IsOdd;
+
+            ExecuteForOneToTen(IsOdd);
+            ExecuteForOneToTen(IsEven);
+
+            Console.WriteLine(myFunc(5));
+
+            Console.ReadKey();
+        }
+
+        static void ExecuteForOneToTen(Predicate f)
+        {
+            for (int i = 1; i <= 10; i++)
+                Console.WriteLine(f(i));
+        }
+
+        static bool IsOdd(int i)
+        {
+            return i % 2 != 0;
+        }
+        static bool IsEven(int i)
+        {
+            return i % 2 == 0;
         }
     }
 }
