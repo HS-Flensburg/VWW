8508da5dc2694868ea73636656bdd81567e1d466
Benjamin Schulz <bschulz@code-area.net>
2016-04-07 15:16:31 +0200

    service implementation

---
+++ VWW_Vorlesung/VWW_V3_WCF.Server/MyUserCheck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using VWW_V3_WCF.Contracts;
+
+namespace VWW_V3_WCF.Server
+{
+    internal class MyUserCheck : IUserCheck
+    {
+        public bool IsAdult(User usr)
+        {
+            return usr.Age >= 18;
+        }
+    }
+}
