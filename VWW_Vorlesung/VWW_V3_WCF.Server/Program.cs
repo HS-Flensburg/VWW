e249e9041903f5bc0188848b5283833a8a7c14bc
Benjamin Schulz <bschulz@code-area.net>
2016-04-07 15:15:12 +0200

    host implementation

--- VWW_Vorlesung/VWW_V3_WCF.Server/Program.cs
+++ VWW_Vorlesung/VWW_V3_WCF.Server/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using VWW_V3_WCF.Contracts;
 
 namespace VWW_V3_WCF.Server
 {
@@ -10,6 +12,21 @@ namespace VWW_V3_WCF.Server
     {
         static void Main(string[] args)
         {
+            ServiceHost host = null;
+            try
+            {
+                host = new ServiceHost(typeof(MyUserCheck));
+                host.AddServiceEndpoint(typeof(IUserCheck), new BasicHttpBinding(), new Uri("http://localhost:8080/test/user"));
+                host.Open();
+                Console.WriteLine("hit enter to exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                if (host != null)
+                    host.Close();
+            }
         }
     }
 }
