bdf9a82f89462831d2545b83b7f80538ceab774a
Benjamin Schulz <bschulz@code-area.net>
2016-04-07 15:06:00 +0200

    Service contract

--- VWW_Vorlesung/VWW_V3_WCF.Contracts/IUserCheck.cs
+++ VWW_Vorlesung/VWW_V3_WCF.Contracts/IUserCheck.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VWW_V3_WCF.Contracts
 {
+    [ServiceContract]
     public interface IUserCheck
     {
+        [OperationContract]
+        [ServiceKnownType(typeof(User))]
         bool IsAdult(User usr);
     }
 }
--- VWW_Vorlesung/VWW_V3_WCF.Contracts/User.cs
+++ VWW_Vorlesung/VWW_V3_WCF.Contracts/User.cs
@@ -4,10 +4,10 @@ using System.Runtime.Serialization;
 namespace VWW_V3_WCF.Contracts
 {
     [DataContract]
-    [KnownType(typeof(Guid))]
+    //[KnownType(typeof(Guid))] nur bei eigenen klassen
     public class User
     {
-        [DataMember]
+        [DataMember] //optional
         public string Name { get; set; }
         [DataMember]
         public int Age { get; set; }
