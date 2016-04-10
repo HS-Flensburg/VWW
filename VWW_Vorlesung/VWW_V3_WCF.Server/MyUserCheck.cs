70ecc81c688e5e4ef63e7e248e777b7353041ced
Benjamin Schulz <bschulz@code-area.net>
2016-04-10 15:37:44 +0200

    authorization via attributes

--- VWW_Vorlesung/VWW_V3_WCF.Server/MyUserCheck.cs
+++ VWW_Vorlesung/VWW_V3_WCF.Server/MyUserCheck.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Security.Permissions;
 using VWW_V3_WCF.Contracts;
 
 namespace VWW_V3_WCF.Server
 {
     internal class MyUserCheck : IUserCheck
     {
+        //[PrincipalPermission(SecurityAction.Demand, Role = "Admin")]
         public bool IsAdult(User usr)
         {
             Console.WriteLine("checking user :" + usr.Name);
