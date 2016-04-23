0b2fa0c836f8a50f3ae9e3360d6136464021375c
Benjamin Schulz <bschulz@code-area.net>
2016-04-23 22:37:34 +0200

    added remoting server-project with implementation of the service contract

---
+++ VWW_Vorlesung/VWW_V5_RemotingDemo.Server/MyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VWW_V5_RemotingDemo.Contracts;
+
+namespace VWW_V5_RemotingDemo.Server
+{
+    class MyService : IMyService
+    {
+        public string GetHallo(string name)
+        {
+            return "Hallo " + name;
+        }
+
+        public User GetUser()
+        {
+            return new User()
+            {
+                Age = 22,
+                Name = "User"
+            };
+        }
+    }
+}
