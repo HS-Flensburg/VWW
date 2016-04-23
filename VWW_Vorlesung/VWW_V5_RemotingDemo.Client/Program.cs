131fd16e98d9c725f9ed28f692af0cd8298cda0d
Benjamin Schulz <bschulz@code-area.net>
2016-04-23 22:41:32 +0200

    added client-project

---
+++ VWW_Vorlesung/VWW_V5_RemotingDemo.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWW_V5_RemotingDemo.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
