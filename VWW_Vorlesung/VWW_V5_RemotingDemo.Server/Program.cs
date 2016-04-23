ad4bfe62ba1fa15fb28c8602faa1e77238a207bd
Benjamin Schulz <bschulz@code-area.net>
2016-04-23 22:40:14 +0200

    added reference to 'System.Runtime.Remoting' added server logic

--- VWW_Vorlesung/VWW_V5_RemotingDemo.Server/Program.cs
+++ VWW_Vorlesung/VWW_V5_RemotingDemo.Server/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,11 @@ namespace VWW_V5_RemotingDemo.Server
     {
         static void Main(string[] args)
         {
+            MyService ser = new MyService();
+            ChannelServices.RegisterChannel(new TcpChannel(8080));
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(MyService), "GetHallo", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Ready...");
+            Console.ReadKey();
         }
     }
 }
