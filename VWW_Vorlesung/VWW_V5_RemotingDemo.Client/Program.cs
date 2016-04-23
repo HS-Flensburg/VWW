0c3afc289013d0b0181e2fd12a6dba990852c870
Benjamin Schulz <bschulz@code-area.net>
2016-04-23 22:45:34 +0200

    added reference: 'System.Runtime.Remoting' added client logic

--- VWW_Vorlesung/VWW_V5_RemotingDemo.Client/Program.cs
+++ VWW_Vorlesung/VWW_V5_RemotingDemo.Client/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
+using VWW_V5_RemotingDemo.Contracts;
 
 namespace VWW_V5_RemotingDemo.Client
 {
@@ -10,6 +13,15 @@ namespace VWW_V5_RemotingDemo.Client
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("press any key to start");
+            Console.ReadKey();
+            ChannelServices.RegisterChannel(new TcpChannel());
+            IMyService ser = (IMyService)Activator.GetObject(typeof(IMyService), "tcp://localhost:8080/GetHallo");
+            Console.WriteLine(ser.GetHallo("Ben"));
+            User u = ser.GetUser();
+            Console.WriteLine(u.Name);
+            u.print("print");
+            Console.ReadKey();
         }
     }
 }
