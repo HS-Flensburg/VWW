f1c4d8db57a2bb57204e3dcc1ff37cf1b108f7a7
Benjamin Schulz <bschulz@code-area.net>
2016-05-23 07:41:05 +0200

    Reflections

---
+++ VWW_Vorlesung/VWW_Reflections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWW_Reflections
+{
+    [Vww(7)]
+    class Sample
+    {
+        public int ID { get; set; }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo");
+            Assembly a = Assembly.GetExecutingAssembly();
+            var p = new Program();
+            foreach (Type t in a.GetTypes())
+            {
+                Console.WriteLine("Type: " + t);
+                foreach (var m in t.GetCustomAttributes())
+                {
+                    if (m is VwwAttribute)
+                    {
+                        VwwAttribute v = (VwwAttribute)m;
+                        Console.WriteLine(v.VL);
+                    }
+                    Console.WriteLine(m);
+                }
+                //Console.WriteLine(t.GetCustomAttribute(typeof(RequiredAttribute)));
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        public void SayHallo()
+        {
+            Console.WriteLine("Moin");
+        }
+
+        public static void SayBye()
+        {
+            Console.WriteLine("Tschüss");
+        }
+    }
+}
---
+++ VWW_Vorlesung/VWW_Reflections/VwwAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWW_Reflections
+{
+    class VwwAttribute : Attribute
+    {
+        public int VL { get; set; }
+
+        public VwwAttribute(int vl)
+        {
+            this.VL = vl;
+        }
+    }
+}
